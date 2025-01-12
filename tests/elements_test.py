751d87d9521a790fbf4ba5ce6e6a84d66e5f638e
MGZykin <mgzykin@yandex.ru>
2025-01-12 22:40:25 +0500

    начал проверку чекбоксов (5.3)

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,11 +1,10 @@
 import time
 
-from pages.elements_page import TextBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage
 from conftest import driver
 
 class TestElements:
     class TestTextBox:
-
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
             text_box_page.open()
@@ -15,3 +14,10 @@ class TestElements:
             assert input_data == output_data, "Почта не совпадает"
             assert input_data == output_data, "Текущий адрес не совпадает"
             assert input_data == output_data, "Постоянный адрес не совпадает"
+
+    class TestCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, "https://demoqa.com/checkbox")
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
